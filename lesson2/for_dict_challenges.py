2d48e31162b9eda678b9f4d8e06c9cc854b3492c
Kate Z <tierinn@gmail.com>
2019-09-15 14:22:56 +0300

    dict

--- lesson2/for_dict_challenges.py
+++ lesson2/for_dict_challenges.py
@@ -7,7 +7,21 @@ students = [
   {'first_name': 'Маша'},
   {'first_name': 'Петя'},
 ]
-# ???
+
+
+def students_count(students):
+    count_names = {}
+    for elem in students:
+        if count_names.get(elem['first_name']) is None:
+            count_names[elem['first_name']] = 1
+        else:
+            count_names[elem['first_name']] += 1
+    return count_names
+
+
+count_names = students_count(students)
+for elem in count_names.keys():
+    print(f'{elem}: {count_names[elem]}')
 
 # Пример вывода:
 # Вася: 1
@@ -24,7 +38,23 @@ students = [
   {'first_name': 'Маша'},
   {'first_name': 'Оля'},
 ]
-# ???
+
+count_names = students_count(students)
+
+
+def find_frequent_name(count_names):
+    frequent_name_count = 0
+    frequent_name = ''
+
+    for elem in count_names.keys():
+        if count_names[elem] > frequent_name_count:
+            frequent_name_count = count_names[elem]
+            frequent_name = elem
+    return frequent_name
+
+
+frequent_name = find_frequent_name(count_names)
+print(f'Самое частое имя среди учеников: {frequent_name}')
 
 # Пример вывода:
 # Самое частое имя среди учеников: Маша
@@ -48,6 +78,11 @@ school_students = [
 # Самое частое имя в классе 1: Вася
 # Самое частое имя в классе 2: Маша
 
+for num, one_class in enumerate(school_students):
+    count_names = students_count(one_class)
+    frequent_name = find_frequent_name(count_names)
+    print(f'Самое частое имя в классе {num + 1}: {frequent_name}')
+
 
 # Задание 4
 # Для каждого класса нужно вывести количество девочек и мальчиков в нём.
@@ -61,9 +96,30 @@ is_male = {
   'Олег': True,
   'Миша': True,
 }
+
+
+def gender_count(one_class):
+    boys_count = 0
+    girls_count = 0
+    for student in one_class['students']:
+        if is_male[student['first_name']] is True:
+            boys_count += 1
+        else:
+            girls_count += 1
+    return one_class["class"], girls_count, boys_count
+
+
+for one_class in school:
+    class_name, girls_count, boys_count = gender_count(one_class)
+
+    print(f'В классе {one_class["class"]} {girls_count} девочки и {boys_count} мальчика.')
+
 # ???
 
+
+
 # Пример вывода:
+
 # В классе 2a 2 девочки и 0 мальчика.
 # В классе 3c 0 девочки и 2 мальчика.
 
@@ -81,6 +137,24 @@ is_male = {
   'Миша': True,
 }
 # ???
+
+max_girls_class = ''
+max_girls_count = 0
+max_boys_class = ''
+max_boys_count = 0
+for one_class in school:
+    class_name, girls_count, boys_count = gender_count(one_class)
+    if girls_count > max_girls_count:
+        max_girls_count = girls_count
+        max_girls_class = class_name
+    if boys_count > max_boys_count:
+        max_boys_count = max_boys_count
+        max_boys_class = class_name
+print(f'Больше всего мальчиков в классе {max_boys_class}')
+print(f'Больше всего мальчиков в классе {max_girls_class}')
+
+
+
 
 # Пример вывода:
 # Больше всего мальчиков в классе 3c
