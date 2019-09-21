2fd58f04ae9a48c0fa560f9eeb2f785e6f91b4cf
Kate Z <tierinn@gmail.com>
2019-09-21 11:33:10 +0300

    bot

--- mybot/bot.py
+++ mybot/bot.py
@@ -1,5 +1,6 @@
 import random
 import logging
+import re
 from datetime import date
 
 import ephem
@@ -17,7 +18,7 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     filename='bot.log'
                     )
 
-with open('ff') as bot_key:
+with open('token_name') as bot_key:
     bot_key = bot_key.read()
     print(bot_key)
 
@@ -65,16 +66,70 @@ def talk_to_me(bot, update):
     update.message.reply_text(items[random. randrange(len(items))])
 
 
+def read_token():
+    with open('token_name', 'r') as f:
+        token = f.readline()
+        return str(token)
+
+
+def word_counter(bot, update):
+    user_text = update.message.text.split(' ')[1:]
+    print(user_text)
+    words_count = 0
+    if len(user_text) == 0:
+        update.message.reply_text('Их нет!')
+    else:
+        for element in user_text:
+            if re.search('[a-zA-Zа-яА-Я0-9]', element):
+                words_count += 1
+        if str(words_count)[-1] == '1':
+            update.message.reply_text(f'{words_count} слово')
+        else:
+            update.message.reply_text(f'{words_count} слова')
+
+
+def next_full_moon(bot, update):
+    user_text = update.message.text.split(' ')[1]
+    user_text = user_text.replace('.', '/')
+    if re.search('(\d\d).(\d\d).(\d{4})', user_text):
+        pattern = re.search('(\d\d).(\d\d).(\d{4})', user_text)
+        user_text = f'{pattern[3]}/{pattern[2]}/{pattern[1]}'
+    full_moon = ephem.next_full_moon(user_text)
+    update.message.reply_text(f'Ближайшее полнолуние: {full_moon}')
+
+
+#def cities(bot, update):
+#    while True:
+#        user_text = ' '.join(update.message.text.split(' ')[1:])
+#        if user_text == 'Хорош':
+#            break
+#        user_cities = []
+#        with open('cities', 'r', encoding='utf-8') as f:
+#            # The protocol version used is detected automatically, so we do not
+#            # have to specify it.
+#            city = ''
+#            for line in f:
+#                if user_text == line:
+#                    city = user_text
+#                    print(city)
+#            if city != '':
+#                for line in f:
+#                    if line[0] == user_text[0].upper():
+#                        update.message.reply_text(line)
+
+
 def main():
     mybot = Updater(
-        token='981618516:AAHrGxYlAegMtnXlB4Ywf5N3dHJv5gAMBvQ',
+        token=read_token(),
         request_kwargs=REQUEST_KWARGS,
     )
-
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     dp.add_handler(CommandHandler('planet', constellation))
+    dp.add_handler(CommandHandler('wordcount', word_counter))
+    dp.add_handler(CommandHandler('next_full_moon', next_full_moon))
+#    dp.add_handler(CommandHandler('cities', cities))
 
 
     mybot.start_polling()
